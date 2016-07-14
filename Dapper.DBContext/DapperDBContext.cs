d375e79fe610a57fe0b5279bf28f4a83f3c610e3
iampkm <chunguoc@163.com>
2016-07-14 16:08:10 +0800

    加入dbcontext

---
+++ Dapper.DBContext/DapperDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper.DBContext
+{
+    /// <summary>
+    /// Dapper 上下文
+    /// </summary>
+   public class DapperDBContext
+    {
+       public DapperDBContext(string connectionString) { 
+       
+       }
+    }
+}
